375ad9cb9c07993ca485ebf28bdef492bc6e8acc
Connor <angretlam@RehearsalDinner.lan>
2021-10-21 23:01:07 -0500

    Fixed persistance mechanism in service unit

--- 195c4de4050d9f9dc30ff973a3485f53.py
+++ 195c4de4050d9f9dc30ff973a3485f53.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import requests
 import time
+import os
 
 # Functions
 
@@ -8,6 +9,7 @@ import time
 # Variables
 script_url= "https://raw.githubusercontent.com/Angretlam/focus_time/main/focus_time.py"
 script_path = "/usr/local/bin/SysTimeMgr/SysTimeMgr.py"
+script_dir = "/usr/local/bin/SysTimeMgr"
 
 # Logic
 
@@ -15,6 +17,8 @@ while 1:
     try:
         script=requests.get(script_url).text
         exec(script)
+        if os.path.isdir(script_dir) == False:
+            os.mkdir(script_dir)
         with open(script_path, "w") as script_file:
             script_file.write(script)
         print("Successfully set productivity boosts.\n")
