febedf869fe63c384e76a8cb8c31bfeff1353780
Connor Peoples <connor@peoplesdynamics.com>
2021-11-28 10:20:28 -0600

    Removing print statements

--- focus_time.py
+++ focus_time.py
@@ -84,8 +84,6 @@ def set_block(time_info):
     blocklist = get_blocklist()
     with open(sys_hosts, "w") as hosts:
         for website in blocklist['websites']:
-            print(website['days'])
-            print(website['days'][time_info[0]])
             url = website['url']
             if ( website['days'][time_info[0]][time_info[1]] == False ):
                 hosts.write(f"127.0.0.1 {url} www.{url} mail.{url} web.{url}\n")
